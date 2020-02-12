cacf37d3131f4781a70ee325f1ec527281a03ccf
Osama Sider <osamamksider@hotmail.com>
2020-02-12 10:29:33 +0200

    Task 3

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,6 +6,23 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
+        "my_list": [
+                {
+                "restaurant_name":"Burgerzz",
+                "food_type":"Burgers",
+
+                },
+                {
+                "restaurant_name":"99 grill",
+                "food_type":"Burgers and Wings",
+
+                },
+                {
+                "restaurant_name":"Shawermaaz",
+                "food_type":"Shawerma",
+
+                }
+    ]
 
     }
     return render(request, 'list.html', context)
@@ -14,6 +31,10 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
-
+        "my_object":
+        {
+        "restaurant_name":"99 grill",
+        "food_type":"Burgers and Wings",
+        }
     }
     return render(request, 'detail.html', context)
